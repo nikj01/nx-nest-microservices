38d6790bd686184f6cd99bf1cf686576a9f0c143
Nik <nik.jurawlyow2001@gmail.com>
2024-03-28 13:24:47 +0200

    feat | library with interfaces was added

--- services/apps/owners/src/app/app.module.ts
+++ services/apps/owners/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { OwnerModule } from "./owner/owner.module";
 
 @Module({
-  imports: [],
+  imports: [OwnerModule],
   controllers: [AppController],
   providers: [AppService],
 })
