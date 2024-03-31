fce10a4eed26c4402a836d9ad3f7d83027123277
Nik <nik.jurawlyow2001@gmail.com>
2024-03-31 20:30:35 +0300

    feat | docker-compose file was added into owners application

--- services/apps/owners/src/app/configs/mongo.config.ts
+++ services/apps/owners/src/app/configs/mongo.config.ts
@@ -10,7 +10,22 @@ export const getMongoConfig = (): MongooseModuleAsyncOptions => {
     imports: [ConfigModule],
   };
 };
+
 const getMongoString = (configService: ConfigService) =>
-  "mongodb://" + configService.get("MONGO_HOST") + ":" + configService.get("MONGO_PORT") + "/";
+  "mongodb://" +
+  configService.get("MONGO_LOGIN") +
+  ":" +
+  configService.get("MONGO_PASSWORD") +
+  "@" +
+  configService.get("MONGO_HOST") +
+  ":" +
+  configService.get("MONGO_PORT");
+// "/" +
+// configService.get("MONGO_DATABASE") +
+// "?authSource=" +
+// configService.get("MONGO_AUTHDATABASE");
+
+// const getMongoString = (configService: ConfigService) =>
+//   "mongodb://" + configService.get("MONGO_HOST") + ":" + configService.get("MONGO_PORT") + "/";
 
 // mongodb://localhost:27017
